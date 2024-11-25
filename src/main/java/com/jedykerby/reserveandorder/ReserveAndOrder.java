cdec797121a408d9cbf42ea81d9771b8235f3926
codingbox <jhonericssonytac365@gmail.com>
2024-11-25 19:19:45 +0800

    first commit

---
+++ src/main/java/com/jedykerby/reserveandorder/ReserveAndOrder.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.jedykerby.reserveandorder;
+
+/**
+ *
+ * @author YTAC
+ */
+public class ReserveAndOrder {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
